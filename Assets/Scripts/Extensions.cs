6ec2912fb5b8f63a6f5a7e14fa84b0912e7554ab
김지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2024-07-04 14:26:40 +0900

    string 데이터 BigInteger로 변경 확장 메서드 작성

--- Assets/Scripts/Extensions.cs
+++ Assets/Scripts/Extensions.cs
@@ -1,9 +1,11 @@
 using System.Numerics;
+using System.Text;
+using Unity.VisualScripting;
 
 public static class BigIntegerExtensions
 {
-    private static readonly string[] Suffixes = { "", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-    private static readonly BigInteger thousand = new BigInteger(1000);
+    private static readonly char[] Suffixes = { ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+    private static readonly BigInteger thousand = thousand = new BigInteger(1000);
 
     public static string FormatBigInteger(this BigInteger value)
     {
@@ -22,5 +24,32 @@ public static class BigIntegerExtensions
         }
 
         return $"{value}.{rem:D3}{Suffixes[suffixIndex]}";
+    }
+}
+
+public static class StringExtensions
+{
+    private static readonly BigInteger thousand = new BigInteger(1000);
+    private static BigInteger significant = default;
+    private static BigInteger floating = default;
+    public static int suffixAscii = 97;
+    public static BigInteger ToBigInteger(this string value)
+    {
+        significant = default;
+
+        char suffix = value[value.Length - 1];
+
+        if(suffix < 97)
+        {
+            return significant = BigInteger.Parse(value);
+        }
+
+        int dotIndex = value.IndexOf('.');
+        significant = BigInteger.Parse(value.Substring(0, dotIndex));
+        floating = BigInteger.Parse(value.Substring(dotIndex + 1, 3));
+
+        int exp = suffix - suffixAscii;
+
+        return significant * BigInteger.Pow(thousand, exp);
     }
 }
