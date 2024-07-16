e6ece8c3828b26e0981a9b7061b7c40aede3e19d
김지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2024-07-16 16:06:39 +0900

    끊겨진 레퍼런스 연결

--- Assets/Scripts/08.Ui/UiCraftingSlot.cs
+++ Assets/Scripts/08.Ui/UiCraftingSlot.cs
@@ -1,13 +1,14 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
 public class UiCraftingSlot : MonoBehaviour
 {
+    private static readonly string format = "{0} / {1}";
     public Image imagePortrait;
     public TextMeshProUGUI textName;
     public TextMeshProUGUI textRemainAmount;
+    public TextMeshProUGUI textRemainProcess;
     public Slider sliderProcess;
     public Button buttonCancel;
     public RecipeStat recipeStat;
@@ -24,12 +25,18 @@ public class UiCraftingSlot : MonoBehaviour
         if (recipeStat == null)
             return;
         this.recipeStat = recipeStat;
-
-        //if(animalData.GetProfile() != null)
-        //    imagePortrait.sprite = animalData.GetProfile();
-
         sliderProcess.minValue = 0f;
         sliderProcess.maxValue = recipeStat.RecipeData.Workload;
-        //sliderProcess.value = recipeStat.Stamina;
+
+        textName.text = this.recipeStat.RecipeData.GetName();
+        textRemainAmount.text = "0"; // 임시 코드
+        textRemainProcess.text = string.Format(sliderProcess.value.ToString(), recipeStat.RecipeData.Workload);
+    }
+
+    public void OnClickCancel()
+    {
+        var uiCraftingTable = UiManager.Instance.craftTableUi;
+        uiCraftingTable.uiRecipeList.Add(recipeStat);
+        Destroy(gameObject);
     }
 }
