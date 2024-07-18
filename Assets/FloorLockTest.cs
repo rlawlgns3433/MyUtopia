f13cd89053d2d2445dfc9b6f591f18cfe06934ef
김지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2024-07-18 22:02:11 +0900

    테스트용 버전

--- Assets/FloorLockTest.cs
+++ Assets/FloorLockTest.cs
@@ -21,7 +21,17 @@ public class FloorLockTest : MonoBehaviour
             CurrencyManager.currency[CurrencyType.CopperIngot] += 50000;
             CurrencyManager.currency[CurrencyType.SilverIngot] += 50000;
             CurrencyManager.currency[CurrencyType.GoldIngot] += 50000;
-            CurrencyManager.currency[CurrencyType.CopperIngot] += 50000;
         }
+    }
+
+    public void ShowMeTheMoney()
+    {
+        CurrencyManager.currency[CurrencyType.Coin] += 10000;
+        CurrencyManager.currency[CurrencyType.CopperStone] += 10000;
+        CurrencyManager.currency[CurrencyType.SilverStone] += 10000;
+        CurrencyManager.currency[CurrencyType.GoldStone] += 10000;
+        CurrencyManager.currency[CurrencyType.CopperIngot] += 10000;
+        CurrencyManager.currency[CurrencyType.SilverIngot] += 10000;
+        CurrencyManager.currency[CurrencyType.GoldIngot] += 10000;
     }
 }
