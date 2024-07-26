31923e1be7934392639ea86eb06df3548d01d517
김정현 <wjdgus04175@gmail.com>
2024-07-26 10:49:08 +0900

    현재 계층 이름 Ui추가(임시)

--- Assets/Scripts/08.Ui/UiMain.cs
+++ Assets/Scripts/08.Ui/UiMain.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,9 +7,11 @@ public class UiMain : MonoBehaviour
     public ScrollRect scrollRect;
     public UiAnimalInventory animalInventoryUi;
     public GameObject addSlot;
-
+    public TextMeshProUGUI currentFloorName;
     public void Refresh()
     {
         addSlot.GetComponent<Transform>().SetAsLastSibling();
+        var floor = FloorManager.Instance.GetCurrentFloor();
+        currentFloorName.text = floor.FloorStat.FloorData.GetFloorName();
     }
 }
