6d8aad2239111b4d5b7711c5d825f97c87ca5c2a
김지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2024-08-03 14:07:42 +0900

    레벨업 시 동물 스테미나 수치 수정
    기존 : 레벨업 전 스테미나의 수치를 유지
    수정 : 레벨업 전 스테미나의 백분율을 유지

--- Assets/Scripts/01.Animal/Animal.cs
+++ Assets/Scripts/01.Animal/Animal.cs
@@ -140,9 +140,9 @@ public class Animal : IGrowable, IMovable
         if (CurrencyManager.currency[CurrencyType.Coin] < lvCoin) // 임시 코드
             return;
 
-        float currentStamina = animalStat.Stamina;
+        float currentStamina = animalStat.Stamina / animalStat.AnimalData.Stamina;
         animalStat.AnimalData = DataTableMgr.GetAnimalTable().Get(animalStat.Animal_ID + 1);
-        animalStat.Stamina = currentStamina;
+        animalStat.Stamina = animalStat.AnimalData.Stamina * currentStamina;
 
         foreach (var a in animals)
         {
