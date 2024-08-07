3ab33d04db2826730cf1e054bf2a693152dd2070
김지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2024-08-07 17:19:07 +0900

    씬병합

--- Assets/ClockFormatTimer.cs
+++ Assets/ClockFormatTimer.cs
@@ -1,17 +1,12 @@
 using UnityEngine;
-using UnityEngine.UI;
 using DG.Tweening;
 using TMPro;
-using Cysharp.Threading.Tasks;
 using System;
 
 public class ClockFormatTimer : MonoBehaviour
 {
     private static readonly string formatTimer = "전체 갱신까지 {0:00}:{1:00}:{2:00}";
     private int timerDuration = 3600; 
-    // 구글 시간으로 내일 00시 00분 00초까지의 시간
-    // 네트워크 연결이 되어 있는 경우 서버 시간을 받아와야 함
-    // 네트워크 연결이 되어 있지 않은 경우 기기의 시간을 받아와야 함
 
     public TextMeshProUGUI timerText;
     private Tween timerTween;
@@ -69,9 +64,7 @@ public class ClockFormatTimer : MonoBehaviour
         DateTime tomorrowMidnight = DateTime.Today.AddDays(1);
 
         TimeSpan durationUntilTomorrow = tomorrowMidnight - DateTime.Now;
-
-        Debug.LogError($"내일까지 {timerDuration = durationUntilTomorrow.Seconds + durationUntilTomorrow.Minutes * 60 + durationUntilTomorrow.Hours * 3600}초");
-
+        timerDuration = durationUntilTomorrow.Seconds + durationUntilTomorrow.Minutes * 60 + durationUntilTomorrow.Hours * 3600;
         return durationUntilTomorrow;
     }
 
